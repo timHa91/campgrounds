e7e52b3c76113c5abf314469263ab904e5bfbffd
Tim Hartmann <45972852+timHa91@users.noreply.github.com>
2022-03-04 12:49:36 +0100

    Add Campground new & create

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const exp = require('constants');
 
 //This is the minimum needed to connect the yelp-camp database
 mongoose.connect('mongodb://localhost:27017/tim-camp', {
@@ -24,6 +25,15 @@ app.set('view engine', 'ejs');
 // path.join adds both parts together = __dirname path + views
 app.set('views', path.join(__dirname, 'views'));
 
+//You NEED express.json() and express.urlencoded() for POST and PUT requests, because in both these requests you are sending
+//data (in the form of some data object) to the server and you are asking the server to accept or store that data (object),
+//which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request 
+//Express provides you with middleware to deal with the (incoming) data (object) in the body of the request.
+//urlencoded is a method inbuilt in express to recognize the incoming Request Object as strings or arrays.
+app.use(express.urlencoded({ extended: true }));
+
+
+
 //The req object represents the HTTP request and has properties for the request query string, parameters, body, and HTTP headers. 
 //The res object represents the HTTP response that an Express app sends when it gets an HTTP request. 
 app.get('/', (req, res) => {
@@ -35,6 +45,19 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    //req.body.campground { title: 'hallo', location: 'hallo' }
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    //campground._id ist die DB Eintrag id
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// Id muss als letztes stehen sonst wird alles als /id gesehen
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
